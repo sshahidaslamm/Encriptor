e6e07e7193b95835dfbe11ac78a352fb84e28935
Shahid Aslam <sshahidaslamm@gmail.com>
2015-09-21 17:41:14 +0500

    Update NSString+hex.h

--- NSString+hex.h
+++ NSString+hex.h
@@ -3,7 +3,7 @@
 //  
 //
 //  Created by Shahid Aslam on 3/9/15.
-//  Copyright (c) 2015 SystemsLtd. All rights reserved.
+//  Copyright (c) 2015. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
